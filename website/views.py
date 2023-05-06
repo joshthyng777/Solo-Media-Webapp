09873c07b8b881f0dc56ae87a88b2288326b68d1
Josh T <joshthyng187@gmail.com>
2023-05-06 10:46:18 -0400

    Commiting local changes

--- website/views.py
+++ website/views.py
@@ -6,13 +6,13 @@ import json
 
 views = Blueprint('views', __name__)
 
-
 @views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
     if request.method == 'POST':
-        note = request.form.get('note')
-        weight = request.form.get('weight')
+        note = request.form.get('note_data')  # updated the field name to match the HTML
+        weight = request.form.get('weight_data')
+        weight_lifted = request.form.get('weight_lifted')
         if note and len(note) < 1:
             flash('Note is too short!', category='error')
         elif weight and len(weight) < 1:
@@ -22,13 +22,12 @@ def home():
                 new_note = Note(data=note, user_id=current_user.id)
                 db.session.add(new_note)
             if weight:
-                new_weight = Weight(weight_data=weight, user_id=current_user.id)
+                new_weight = Weight(weight_data=weight, weight_lifted=weight_lifted, user_id=current_user.id)
                 db.session.add(new_weight)
             db.session.commit()
             flash('Information added!', category='success')
     return render_template("home.html", user=current_user)
 
-@views.route('/delete-note', methods=['POST'])
 def delete_note():  
     note = json.loads(request.data) # this function expects a JSON from the INDEX.js file 
     noteId = note['noteId']
